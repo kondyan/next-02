b4920c3a582a7956897de2626558ba4b58e277f0
Arsenii Kondratenko <arsenij.kondratenko@gmail.com>
2025-06-22 15:11:26 +0200

    0-commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
+};
+
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}

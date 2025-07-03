97459bd10984d833a21771a556a2bd75b3a3ab09
Arsenii Kondratenko <arsenij.kondratenko@gmail.com>
2025-07-03 08:39:20 +0200

    9-commit

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,46 +1,12 @@
-"use client";
+import NewsList from "@/components/news-list/news-list";
+import { getAllNews } from "@/lib/news";
 
-import { useEffect, useState } from "react";
-import NewsList from "../../../components/news-list/news-list";
-
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [news, setNews] = useState();
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localohost:8080");
-
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-      }
-
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+export default async function NewsPage() {
+  const news = getAllNews();
   return (
     <main>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </main>
   );
 }
